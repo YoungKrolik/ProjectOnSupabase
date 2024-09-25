f2befb23b4f7d5901af59d31bac1d714ed72b70f
DenisPavlov <faram3030@gmail.com>
2024-09-25 16:07:52 +0300

    Added function LoginScreen.kt and imported to MainActivity.kt

--- app/src/main/java/com/example/projectonsupabase/MainActivity.kt
+++ app/src/main/java/com/example/projectonsupabase/MainActivity.kt
@@ -4,29 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.projectonsupabase.ui.theme.ProjectOnSupabaseTheme
+import com.example.projectonsupabase.ui.theme.ui.LoginScreen
 
-class MainActivity : ComponentActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ProjectOnSupabaseTheme {
+            LoginScreen { email, password ->
 
 
-                }
             }
         }
+    }
+
+
 }
 
 
---
+++ app/src/main/java/com/example/projectonsupabase/ui/theme/ui/LoginScreen.kt
@@ -0,0 +1,62 @@
+package com.example.projectonsupabase.ui.theme.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(onLogin: (String, String) -> Unit)
+{
+    var email by remember {
+     mutableStateOf("")}
+    var password by remember{
+        mutableStateOf("")
+    }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login", style = MaterialTheme.typography.bodyMedium)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        
+        Spacer(modifier =
+        Modifier.height(16.dp))
+        
+        Button(
+            onClick = { 
+                onLogin(email, password) //Вызываем коллбек при клике на кнопку
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+           Text("Login")
+        }
+    }
+}
